6a1a80c5de267131d4c0062c63d8b57048f5fd72
oratta <oratta@gmail.com>
2024-08-10 10:25:20 +0900

    ページをリロードしてもjobIdが消えないようにローカル保存

--- src/commons/components/elements/ContentProvider.js
+++ src/commons/components/elements/ContentProvider.js
@@ -1,10 +1,24 @@
 'use client';
 
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import JobProcessingContext from '@/commons/contexts/JobProcessingContext';
 
 export default function ContentProvider({children}) {
     const [processingJobId, setProcessingJobId] = useState(null);
+    useEffect(() => {
+        const storedJobId = localStorage.getItem('processingJobId');
+        if (storedJobId) {
+            setProcessingJobId(storedJobId);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (processingJobId) {
+            localStorage.setItem('processingJobId', processingJobId);
+        } else {
+            localStorage.removeItem('processingJobId');
+        }
+    }, [processingJobId]);
 
     return (
         <JobProcessingContext.Provider value={{processingJobId, setProcessingJobId}}>

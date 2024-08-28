e55c6670a0f70b9a8aeaf0c06a6a0e576ad16fa9
oratta <oratta@gmail.com>
2024-08-28 12:00:09 +0900

    表示の微調整

--- src/app/order/OrderList.js
+++ src/app/order/OrderList.js
@@ -15,6 +15,7 @@ export default function OrderList({ orderList, onResume}) {
     }, [orderList, resumeThresholdHours]);
 
     const checkResumeCondition = (jobs) => {
+        if(jobs.length === 0) return false;
         const execRunningJob = jobs.find(job => job.status === JOB_LISTING_STATUS.EXEC_RUNNING);
         if (!execRunningJob) return true;
 
@@ -96,7 +97,7 @@ export default function OrderList({ orderList, onResume}) {
         : orderList.filter(order => order.status !== JOB_LISTING_STATUS.EXEC_COMPLETED);
 
     return (
-        <div className="mt-8 w-full relative">
+        <div className="mt-8 w-full">
             <div className="flex justify-between items-center mb-4">
                 <h2 className="text-xl font-semibold mb-4 flex items-center">
                     <ClipboardList className="mr-2" size={24}/>
@@ -115,7 +116,7 @@ export default function OrderList({ orderList, onResume}) {
                     </label>
                 </div>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4">
+            <div className="relative grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4">
                 {Array.isArray(filteredOrderList) && filteredOrderList.map((order) => {
                     if (!order || typeof order !== 'object') {
                         console.error('Invalid order object:', order);
@@ -188,24 +189,25 @@ export default function OrderList({ orderList, onResume}) {
                         </div>
                     );
                 })}
-            </div>
-            {showResumeOverlay && (
-                <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center rounded-2xl">
-                    <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center">
-                        <button
-                            onClick={handleResumeClick}
-                            className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow-lg hover:bg-blue-600 transition-colors duration-300 flex items-center mb-4"
-                        >
-                            <RefreshCw className="mr-2" size={24}/>
-                            Resume Jobs
-                        </button>
-                        <p className="text-white text-center max-w-md px-4">
-                            It seems the investigation has been interrupted midway. Please press the Resume button to
-                            continue.
-                        </p>
+                {showResumeOverlay && (
+                    <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center rounded-2xl">
+                        <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center">
+                            <button
+                                onClick={handleResumeClick}
+                                className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow-lg hover:bg-blue-600 transition-colors duration-300 flex items-center mb-4"
+                            >
+                                <RefreshCw className="mr-2" size={24}/>
+                                Resume Jobs
+                            </button>
+                            <p className="text-white text-center max-w-md px-4">
+                                It seems the investigation has been interrupted midway. Please press the Resume button to
+                                continue.
+                            </p>
+                        </div>
                     </div>
-                </div>
-            )}
+                )}
+            </div>
+
         </div>
     );
 }

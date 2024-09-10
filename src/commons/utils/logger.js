f122daa9dab80f72d836fcce8b2ebc6993d35776
oratta <oratta@gmail.com>
2024-09-10 14:49:30 +0900

    ログ出力先を標準出力に変更

--- src/commons/utils/logger.js
+++ src/commons/utils/logger.js
@@ -10,9 +10,14 @@ const logger = winston.createLogger({
         winston.format.json()
     ),
     defaultMeta: { service: 're-reco' },
+    // transports: [
+    //     new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    //     new winston.transports.File({ filename: 'logs/combined.log' }),
+    // ],
     transports: [
-        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'logs/combined.log' }),
+        new winston.transports.Console({
+            format: winston.format.simple(),
+        }),
     ],
 });
 

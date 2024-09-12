15b2a3eef1e582ad115cf07582306368f4c06a08
oratta <oratta@gmail.com>
2024-09-12 16:51:04 +0900

    ビルドエラー対策
    winstonをserver側だけで使うように修正

--- src/commons/utils/logger.js
+++ src/commons/utils/logger.js
@@ -1,38 +1,50 @@
-import winston from 'winston';
 import {consoleError, consoleLog} from "@/commons/utils/log";
 
-const logger = winston.createLogger({
-    level: process.env.NODE_ENV === 'production' ? 'warn' : 'info',
-    format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.errors({ stack: true }),
-        winston.format.splat(),
-        winston.format.json()
-    ),
-    defaultMeta: { service: 're-reco' },
-    // transports: [
-    //     new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-    //     new winston.transports.File({ filename: 'logs/combined.log' }),
-    // ],
-    transports: [
-        new winston.transports.Console({
-            format: winston.format.simple(),
-        }),
-    ],
-});
+const isServer = typeof window === 'undefined';
+let logger;
 
-if (process.env.NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console({
+if (isServer) {
+    const winston = require('winston');
+
+    logger = winston.createLogger({
+        level: process.env.NODE_ENV === 'production' ? 'warn' : 'info',
         format: winston.format.combine(
-            winston.format.colorize(),
-            winston.format.simple()
+            winston.format.timestamp(),
+            winston.format.errors({ stack: true }),
+            winston.format.splat(),
+            winston.format.json()
         ),
-    }));
+        defaultMeta: { service: 're-reco' },
+        // transports: [
+        //     new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+        //     new winston.transports.File({ filename: 'logs/combined.log' }),
+        // ],
+        transports: [
+            new winston.transports.Console({
+                format: winston.format.simple(),
+            }),
+        ],
+    });
+
+    if (process.env.NODE_ENV !== 'production') {
+        logger.add(new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple()
+            ),
+        }));
+    }
+// Helper functions
+
+}else {
+    logger = {
+        info: console.info,
+        warn: console.warn,
+        error: console.error,
+        debug: console.info
+    }
 }
 
-export default logger;
-
-// Helper functions
 export function debugMsg(message, meta = {}) {
     logger.debug(message, { ...meta, caller: getCaller() });
     consoleLog(message);
@@ -63,3 +75,6 @@ function getCaller() {
     }
     return callerFrame;
 }
+
+export default logger;
+
